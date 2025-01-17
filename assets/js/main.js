c81fe7a91931aa1b691f1a794899e11d07faabaf
noblecovn <noblecovn@gmail.com>
2025-01-17 16:55:25 +0700

    10

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,17 @@
-// khi click vaof button work with us hiện ra form
-const buttonWorkWithUs = document.querySelectorAll(".button.on--nav");
-const form = document.querySelector(".form__popup");
-const pre = document.querySelector(".previous-button");
-const next = document.querySelector(".next.w-slider-arrow-right");
-const step1 = document.querySelector('[aria-label="step1"]');
-const step2 = document.querySelector('[aria-label="step2"]');
-buttonWorkWithUs.forEach((button) => {
-    button.addEventListener("click", () => {
-        // tắt display none
-        form.style.display = "block";
-        step1.style.display = "block";
-        step2.style.display = "none";
-        pre.style.display = "none";
-        next.style.display = "block";
+const closePopup = document.querySelectorAll('.pop__close');
+const popup = document.querySelectorAll('.cc__popup');
+const title = document.querySelectorAll('.cc_title');
+
+title.forEach((title, index) => {
+    title.addEventListener('click', () => {
+        popup[index].style.display = 'flex';
     });
-});
-// khi click vào close__p thì form ẩn đi
-const closePopup = document.querySelector(".close__p");
-closePopup.addEventListener("click", () => {
-    form.style.display = "none";
-});
-next.addEventListener("click", () => {
-    step1.style.display = "none";
-    step2.style.display = "block";
-    pre.style.display = "block";
-    next.style.display = "none";
-});
-pre.addEventListener("click", () => {
-    step1.style.display = "block";
-    step2.style.display = "none";
-    pre.style.display = "none";
-    next.style.display = "block";
 });
 
-document.getElementById("wf-form-Apply-Form").addEventListener("submit", function (event) {
-    event.preventDefault(); // Ngăn chặn hành động mặc định của form (submit)
-  
-    const formData = new FormData(this); // Lấy toàn bộ dữ liệu form
-    const formValues = {};
-  
-    // Lặp qua formData để lưu vào object
-    formData.forEach((value, key) => {
-      formValues[key] = value;
+closePopup.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        if (popup[index]) { // Đảm bảo rằng popup tại index tồn tại
+            popup[index].style.display = 'none';
+        }
     });
-  
-    console.log(formValues); // Hiển thị toàn bộ giá trị của form
-  });
-  
+});
